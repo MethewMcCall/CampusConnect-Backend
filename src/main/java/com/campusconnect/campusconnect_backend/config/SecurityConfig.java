2dd2d6f480a26dd5a6cc4c13167948cfa4f0b907
MethewMcCall <skshah3005@gmail.com>
2025-03-10 00:59:15 +0530

    Updated some code

---
+++ src/main/java/com/campusconnect/campusconnect_backend/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.campusconnect.campusconnect_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager() throws Exception {
+        return http.getSharedObject(AuthenticationManager.class);
+    }
+}
+
